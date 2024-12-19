abc11d5c782562b9a14281fc72423423749627a0
Bolrock <bolrock@fake.com>
2024-12-19 11:44:17 +0100

    onboarding

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack } from "expo-router";
+import ScreenOne from "../screens/ScreenOne";
 
 const RooLayout = () => {
+    const [value, setValue] = useState(true);
+
+    if (value) return <ScreenOne />;
+
     return (
         <Stack>
             <Stack.Screen name="index" options={{ title: "Home" }} />
             <Stack.Screen name="one" options={{ title: "First" }} />
             <Stack.Screen name="two" options={{ title: "Second" }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
     );
 };
